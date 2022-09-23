2ed8d1849ad1219277afd535d6ff905611b47f9c
tman57 <70616151+dez03@users.noreply.github.com>
2022-09-23 08:18:30 -0400

    added google analytics (i think)

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop';
 import ArchivedNewsletters from "./pages/ArchivedNewsletters"
 import Partners from "./pages/Partners"
-
-
+import ReactGA from 'react-ga';
+import { Component } from "react";
 
 // TODO fix the buttons and months on the calendar for small mobile
 // TODO add social icons to the top nav bar
@@ -29,8 +29,20 @@ import Partners from "./pages/Partners"
 // TODO Style the partners page like https://tokeyclub.com/partnerships
 // TODO finish mission page
 // TODO add contact info on officer page
-function App() {
-  return (
+
+
+
+
+class App extends Component {
+  setGA = () => {
+    ReactGA.initialize('G-ZMNH42F7NE');
+    ReactGA.pageview('Init page view');
+  };
+  componentDidMount(){
+    this.setGA();
+  }
+render() {
+    return (
     <Router>
       <ScrollToTop />
       <div className="App bg-[#F8F0E3] ">
@@ -61,9 +73,45 @@ function App() {
       </div>
     </Router>
   );
+  }
 }
-
 export default App;
+
+// function App() {
+//   return (
+//     <Router>
+//       <ScrollToTop />
+//       <div className="App bg-[#F8F0E3] ">
+//         <div>
+//           <TopNav />
+//           <NavBar />
+
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/hours" element={<Hours />} />
+//             <Route path="/calendar" element={<Calendar />} />
+//             <Route path="/districtproject" element={<DistrictProject />} />
+//             <Route path="/mission" element={<Mission />} />
+//             <Route path="/dues" element={<Dues />} />
+//             <Route path="/members" element={<Members />} />
+//             <Route path="/gallery" element={<Gallery />} />
+//             <Route path="/newsletters" element={<Newsletters />} />
+//             <Route path="/advisors" element={<Advisors />} />
+//             <Route path="/officers" element={<Officers />} />
+//             <Route path="/partners" element={<Partners />} />
+//             <Route
+//               path="/ArchivedNewsletters"
+//               element={<ArchivedNewsletters />}
+//             />
+//           </Routes>
+//           <Footer />
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
 /* 
 <div className="App bg-[#F8F0E3]">
